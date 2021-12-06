57ed4d96b6b7dc7342ee06edb51e2f64445b2d89
LayaTaal <jason@zinnfinity.com>
2021-12-05 23:38:25 -0500

    added content and improved styles

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ import { Box } from '@mui/system';
 import backgroundImage from '../public/background.jpg';
 
 export default function Home() {
-  const [treeHeight, setTreeHeight] = useState(0);
-  const [treeWidth, setTreeWidth] = useState(0);
-  const [rowsOfLights, setRowsOfLights] = useState(0);
-  const [topOffset, setTopOffset] = useState(0);
+  const [treeHeight, setTreeHeight] = useState();
+  const [treeWidth, setTreeWidth] = useState();
+  const [rowsOfLights, setRowsOfLights] = useState();
+  const [topOffset, setTopOffset] = useState();
   const [lengthOfLights, setLengthOfLights] = useState(0);
 
   function Calculator(
@@ -46,7 +46,7 @@ export default function Home() {
   }
 
   return (
-    <div className={styles.container}>
+    <div className={styles.wrapper}>
       <Head>
         <title>
           How many lights do I need for my Christmas tree?
@@ -64,92 +64,162 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <Container maxWidth="xl">
-          <Grid container spacing={4}>
-            <Grid item xs={12} lg={8}>
-              <h1 className={styles.title}>
-                How many lights do I need for my Christmas tree?
-              </h1>
-              <p className={styles.description}>
-                Well...let{"'"}s find out!
-              </p>
+        <div className={styles.upper}>
+          <Container maxWidth="xl">
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={8}>
+                <h1 className={styles.title}>
+                  How many lights do I need for my Christmas tree?
+                </h1>
+                <p className={styles.description}>
+                  Every Christmas tree has to have some sort of lights
+                  on it. Lights come in all kinds of varieties, from
+                  solid colors to multi-color, blinking, and so on.
+                  The list is exhaustive. When purchasing lights for
+                  your tree, it can be difficult to know just what you
+                  need, and especially what length you need!
+                </p>
+                <h2 className={styles.h2}>Fear Not!</h2>
+                <p className={styles.description}>
+                  This simple tool will help you get a good estimate
+                  of the length of lights you should plan to get. All
+                  you need is a tape measure and a few dimensions:
+                </p>
+                <ul>
+                  <li>
+                    The height of your tree from the bottom branch to
+                    the top
+                  </li>
+                  <li>
+                    The width of your tree from tip to tip of the
+                    bottom branches
+                  </li>
+                  <li>An idea of how many rows of lights you want</li>
+                </ul>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container spacing={4}>
-            <Grid item xs={12} md={6} lg={4}>
-              <form
-                className={styles.form}
-                onSubmit={(e) =>
-                  Calculator(e, {
-                    treeHeight,
-                    treeWidth,
-                    rowsOfLights,
-                    topOffset,
-                  })
-                }
-              >
-                <TextField
-                  type="text"
-                  id="outlined-name"
-                  label="Tree Height (ft.)"
-                  helperText="How tall is your tree?"
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={treeHeight}
-                  onChange={(e) => setTreeHeight(e.target.value)}
-                />
-                <TextField
-                  type="text"
-                  id="outlined-name"
-                  label="Tree Width (ft.)"
-                  helperText="How wide is your tree at the lowest branch?"
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={treeWidth}
-                  onChange={(e) => setTreeWidth(e.target.value)}
-                />
-                <TextField
-                  type="text"
-                  id="outlined-name"
-                  label="Rows of Lights"
-                  helperText="How many rows of lights do you want?"
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={rowsOfLights}
-                  onChange={(e) => setRowsOfLights(e.target.value)}
-                />
-                <TextField
-                  type="text"
-                  id="outlined-name"
-                  label="Top Offset (ft.)"
-                  helperText="How far from the top of the tree do you want to start lights?"
-                  margin="normal"
-                  required
-                  fullWidth
-                  value={topOffset}
-                  onChange={(e) => setTopOffset(e.target.value)}
-                />
-                <Button
-                  type="submit"
-                  variant="contained"
-                  size="large"
+            <Grid
+              container
+              spacing={4}
+              className={styles.formContainer}
+            >
+              <Grid item xs={12} md={6} lg={4}>
+                <h2 className={styles.h2}>Get Your Tree Height</h2>
+                <p>
+                  Once you have these numbers ready, input them in the
+                  calculator below and get an estimate of exactly what
+                  you need.
+                </p>
+                <form
+                  className={styles.form}
+                  onSubmit={(e) =>
+                    Calculator(e, {
+                      treeHeight,
+                      treeWidth,
+                      rowsOfLights,
+                      topOffset,
+                    })
+                  }
                 >
-                  Submit
-                </Button>
-              </form>
+                  <TextField
+                    type="text"
+                    id="outlined-name"
+                    label="Tree Height (ft.)"
+                    helperText="How tall is your tree?"
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={treeHeight}
+                    onChange={(e) => setTreeHeight(e.target.value)}
+                  />
+                  <TextField
+                    type="text"
+                    id="outlined-name"
+                    label="Tree Width (ft.)"
+                    helperText="How wide is your tree at the lowest branch?"
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={treeWidth}
+                    onChange={(e) => setTreeWidth(e.target.value)}
+                  />
+                  <TextField
+                    type="text"
+                    id="outlined-name"
+                    label="Rows of Lights"
+                    helperText="How many rows of lights do you want?"
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={rowsOfLights}
+                    onChange={(e) => setRowsOfLights(e.target.value)}
+                  />
+                  <TextField
+                    type="text"
+                    id="outlined-name"
+                    label="Top Offset (ft.)"
+                    helperText="How far from the top of the tree do you want to start lights?"
+                    margin="normal"
+                    required
+                    fullWidth
+                    value={topOffset}
+                    onChange={(e) => setTopOffset(e.target.value)}
+                  />
+                  <Button
+                    type="submit"
+                    variant="contained"
+                    size="large"
+                  >
+                    Submit
+                  </Button>
+                </form>
+              </Grid>
+              <Grid item xs={12} md={6} lg={8}>
+                {lengthOfLights !== 0 && (
+                  <Box className={styles.card}>
+                    <Result lengthOfLights={lengthOfLights} />
+                  </Box>
+                )}
+              </Grid>
             </Grid>
-            <Grid item xs={12} md={6} lg={8}>
-              {lengthOfLights !== 0 && (
-                <Box className={styles.card}>
-                  <Result lengthOfLights={lengthOfLights} />
-                </Box>
-              )}
+          </Container>
+        </div>
+        <div className={styles.lower}>
+          <Container maxWidth="xl">
+            <Grid container spacing={4}>
+              <Grid item xs={12} md={8}>
+                <h2 className={styles.h2}>How Does it Work?</h2>
+                <p className={styles.description}>
+                  This is all pretty simple really, just basic right
+                  triangle geometry. The key is knowing the height of
+                  the tree and the width at the base. Once we know
+                  those two things, we can calculate the opening angle
+                  of the tree. I then use that to build rows and
+                  calculate the circumference at each row. This is
+                  most certainly a rough approximation, but it should
+                  be close enough for this purpose.
+                </p>
+                <h2 className={styles.h2}>Why did I do this?</h2>
+                <p className={styles.description}>
+                  Unfortunately, I went through the process of getting
+                  a tree and lights, without thinking about just how
+                  many I needed. Halfway through putting them on, I
+                  realized I needed at least a third more to cover the
+                  entire tree. I started to think about how to figure
+                  this out in the future and decided to build a little
+                  tool to help myself and hopefully someone else!
+                </p>
+                <h2 className={styles.h2}>Do I Plan Improvements?</h2>
+                <p className={styles.description}>
+                  Yes! I hope to make this a bit more robust in the
+                  future - including being able to change units from
+                  feet to something else, and selecting either a
+                  number of rows or the space between rows.
+                </p>
+              </Grid>
             </Grid>
-          </Grid>
-        </Container>
+          </Container>
+        </div>
       </main>
 
       <footer className={styles.footer}>
